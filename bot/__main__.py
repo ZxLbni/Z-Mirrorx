4afcf7badc52311bb4dbf6752e797fc4425c5b7a
⤷ßñï 🪽 <101978891+ZxLbni@users.noreply.github.com>
2024-05-06 01:12:01 +0000

    Add files via upload

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,10 +58,10 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Ownert: @x_O4i'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
-        start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
+        start_string = 'This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram., you cannot use me here!\n' \
+                       'Owner: @x_O4i .\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
